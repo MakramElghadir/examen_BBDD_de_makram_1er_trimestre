d69e6de4fc93d41c526265cb9812c3789c200c28
Makram <Mkelghadir@gmail.com>
2023-12-19 13:04:22 +0100

    oh y estas tambien

--- 01_practica/script.js
+++ 01_practica/script.js
@@ -1,1 +1,13 @@
 console.log("script.js online")
+
+function añadir() {
+    console.log("input on añadir working")
+}
+
+function borrar() {
+    console.log("input on borrar working")
+}
+
+function sortear() {
+    console.log("input on sortear working")
+}
